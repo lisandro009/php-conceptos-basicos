09ceed096c7321851e2842b62609f52356572d7c
lisandro009 <cristianmdz009@gmail.com>
2022-07-27 21:25:30 -0600

    configuracion parte 2

--- phpMVC/Libs/controller.php
+++ phpMVC/Libs/controller.php
@@ -1,5 +1,20 @@
 <?php
 
 class Controller{
+    protected $view;
+    protected $model;
     
+    function __construct(){
+        $this->view= new View();
+    }
+
+    public function loadModel($model){
+        $ruta= 'Models/'.$model.'Model.php';
+
+        if(file_exists($ruta)){
+            require $ruta;
+            $modelName=$model.'Model';
+            $this->model= new $model();
+        }
+    }
 }
---
+++ phpMVC/Libs/view.php
@@ -0,0 +1,8 @@
+<?php
+
+class View{
+
+    public function render(){
+        require 'views/'.$nombre.'.php';
+    }
+}
